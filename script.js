d88026a1551a4c09879a7fdf7ee0a18bc4853dde
Aigerim Zhusubalieva <az2177@nyu.edu>
2022-05-10 19:18:54 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var welcomeButton = document.querySelector(".welcomeButton");
+var landingPage = document.querySelector(".landingPage");
+
+AOS.init();
+
+welcomeButton.addEventListener("click", function(){
+    landingPage.classList.add("closeLanding");
+});
+
