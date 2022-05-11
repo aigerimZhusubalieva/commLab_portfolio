93fe5f1fd9e83c3631dda88163997bd791960145
Aigerim Zhusubalieva <az2177@nyu.edu>
2022-05-11 21:16:23 +0400

    final commit (hopefully)

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var welcomeButton = document.querySelector(".welcomeButton");
 var landingPage = document.querySelector(".landingPage");
 
 AOS.init();
 
-welcomeButton.addEventListener("click", function(){
+landingPage.addEventListener("click", function(){
     landingPage.classList.add("closeLanding");
 });
